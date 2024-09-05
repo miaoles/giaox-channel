3335f18398a2a0c8a9e509c7b8fe70ef7c5c07f6
miaoles <iao_mm@pm.me>
2024-09-05 08:14:17 -0400

    file-organizer fix attempt #4

--- giaox/services/file-organizer.scm
+++ giaox/services/file-organizer.scm
@@ -30,61 +30,62 @@
 
 (define (file-organizer-activation config)
  #~(begin
-  (use-modules (ice-9 threads))
-  (define (get-file-type file-name file-types)
-   (let ((extension (string-downcase (file-extension file-name))))
-    (find (lambda (type)
-     (member extension (cdr type)))
-     file-types)))
-  (define (move-file source dest)
-   (let ((dir (dirname dest)))
-    (unless (file-exists? dir)
-     (mkdir-p dir))
-    (rename-file source dest)))
-  (define (organize-files source-dir file-types)
-   (let ((files (scandir source-dir)))
-    (for-each
-     (lambda (file)
-      (let* ((full-path (string-append source-dir "/" file))
-       (file-type (and (file-regular? full-path)
-        (get-file-type file file-types))))
-      (when file-type
-       (let ((dest-dir (car (cdr file-type))))
-        (move-file full-path (string-append dest-dir "/" file))))))
-     files)))
-  (define (watch-and-organize config)
-   (let ((watch-dirs '#$(file-organizer-configuration-watch-directories config))
-    (file-types '#$(file-organizer-configuration-file-types config))
-    (interval #$(file-organizer-configuration-interval config)))
-    (let loop ()
-     (for-each (lambda (dir) (organize-files dir file-types)) watch-dirs)
-     (sleep interval)
-     (loop))))
-  (call-with-new-thread
-   (lambda ()
-    (watch-and-organize '#$config)))))
+    (use-modules (ice-9 threads))
+    (define (get-file-type file-name file-types)
+     (let ((extension (string-downcase (file-extension file-name))))
+      (find (lambda (type)
+             (member extension (cdr type)))
+            file-types)))
+    (define (move-file source dest)
+     (let ((dir (dirname dest)))
+      (unless (file-exists? dir)
+       (mkdir-p dir))
+      (rename-file source dest)))
+    (define (organize-files source-dir file-types)
+     (let ((files (scandir source-dir)))
+      (for-each
+       (lambda (file)
+        (let* ((full-path (string-append source-dir "/" file))
+               (file-type (and (file-regular? full-path)
+                               (get-file-type file file-types))))
+         (when file-type
+          (let ((dest-dir (car (cdr file-type))))
+           (move-file full-path (string-append dest-dir "/" file))))))
+       files)))
+    (define (watch-and-organize config)
+     (let ((watch-dirs #$(file-organizer-configuration-watch-directories config))
+           (file-types #$(file-organizer-configuration-file-types config))
+           (interval #$(file-organizer-configuration-interval config)))
+      (let loop ()
+       (for-each (lambda (dir) (organize-files dir file-types)) watch-dirs)
+       (sleep interval)
+       (loop))))
+    (call-with-new-thread
+     (lambda ()
+      (watch-and-organize #$config)))))
 
 (define file-organizer-service-type
- (service-type
-  (name 'file-organizer)
-  (description "Run file organizer service.")
-  (extensions
-   (list (service-extension
-    home-shepherd-service-type
-    file-organizer-shepherd-service)
-    (service-extension
-     home-activation-service-type
-     file-organizer-activation)))
-  (default-value (file-organizer-configuration))
-  (compose identity)  ; Changed from concatenate to identity
-  (extend
-   (lambda (config extra-config)
-    (file-organizer-configuration
-     (watch-directories
-      (append (file-organizer-configuration-watch-directories config)
-       (file-organizer-configuration-watch-directories extra-config)))
-     (file-types
-      (append (file-organizer-configuration-file-types config)
-       (file-organizer-configuration-file-types extra-config)))
-     (interval
-      (file-organizer-configuration-interval extra-config)))))))
+  (service-type
+   (name 'file-organizer)
+   (description "Run file organizer service.")
+   (extensions
+    (list (service-extension
+           home-shepherd-service-type
+           file-organizer-shepherd-service)
+          (service-extension
+           home-activation-service-type
+           file-organizer-activation)))
+   (default-value (file-organizer-configuration))
+   (compose identity)
+   (extend
+    (lambda (config extra-config)
+      (file-organizer-configuration
+       (watch-directories
+        (append (file-organizer-configuration-watch-directories config)
+                (file-organizer-configuration-watch-directories extra-config)))
+       (file-types
+        (append (file-organizer-configuration-file-types config)
+                (file-organizer-configuration-file-types extra-config)))
+       (interval
+        (or (file-organizer-configuration-interval extra-config)
+            (file-organizer-configuration-interval config))))))))
