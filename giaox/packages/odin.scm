52f84afc3f3a22f482211cece3eca17a0050d913
miaoles <iao_mm@pm.me>
2025-02-11 00:47:54 -0500

    odin package revision for lax linking

--- giaox/packages/odin.scm
+++ giaox/packages/odin.scm
@@ -1,12 +1,12 @@
 (define-module (giaox packages odin)
   #:use-module (guix packages)
-  #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
   #:use-module (guix licenses)
   #:use-module (gnu packages llvm)
   #:use-module (gnu packages base)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages gcc)
   #:use-module (srfi srfi-1))
 
 (define-public odin
@@ -24,10 +24,10 @@
         (base32 "17xgyr0xsg3bdfn472kniyld813vprm8g0x99qhj05w8wgirlxqr"))))
     (build-system gnu-build-system)
     (arguments
-     `(#:tests? #f                     ; No standard test target
+     `(#:tests? #f
        #:phases
        (modify-phases %standard-phases
-         (delete 'configure)            ; no configure script
+         (delete 'configure)
          (replace 'build
            (lambda* (#:key inputs #:allow-other-keys)
              (setenv "LLVM_CONFIG"
@@ -38,51 +38,42 @@
                                   "/bin/clang++"))
              (invoke "sh" "build_odin.sh" "release")))
          (replace 'install
-           (lambda* (#:key outputs #:allow-other-keys)
+           (lambda* (#:key inputs outputs #:allow-other-keys)
              (let* ((out (assoc-ref outputs "out"))
                     (bin (string-append out "/bin"))
                     (share (string-append out "/share")))
+               ;; Install main executable and libraries
                (install-file "odin" bin)
                (for-each
                 (lambda (dir)
                   (copy-recursively dir (string-append share "/" dir)))
                 '("base" "core" "vendor" "shared"))
-               #t)))
-         (add-after 'install 'compile-vendor-libraries
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (share (string-append out "/share"))
-                    (vendor (string-append share "/vendor")))
-               ;; Compile cgltf
-               (with-directory-excursion (string-append vendor "/cgltf/src")
+
+               ;; Set compiler environment for vendor compilation
+               (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
+
+               ;; Compile core vendor libraries
+               (with-directory-excursion (string-append share "/vendor/cgltf/src")
                  (invoke "make"))
-               ;; Compile stb
-               (with-directory-excursion (string-append vendor "/stb/src")
+               (with-directory-excursion (string-append share "/vendor/stb/src")
                  (invoke "make"))
-               ;; Compile miniaudio
-               (with-directory-excursion (string-append vendor "/miniaudio/src")
+               (with-directory-excursion (string-append share "/vendor/miniaudio/src")
                  (invoke "make"))
-               #t)))
-         (add-after 'compile-vendor-libraries 'wrap-program
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             ;; Wrap the 'odin' binary to find clang and other tools.
-             (let* ((out (assoc-ref outputs "out"))
-                    (clang-toolchain (assoc-ref inputs "clang-toolchain"))
-                    (llvm (assoc-ref inputs "llvm")))
-               (wrap-program (string-append out "/bin/odin")
-                 `("PATH" prefix (,(string-append clang-toolchain "/bin")))
-                 `("LIBRARY_PATH" prefix (,(string-append clang-toolchain "/lib")))
-                 `("ODIN_ROOT" = (,(string-append out "/share"))))
-               #t))))))
+
+               ;; Create wrapper with necessary environment
+               (wrap-program (string-append bin "/odin")
+                 `("PATH" prefix
+                   (,(string-append (assoc-ref inputs "clang-toolchain") "/bin")))
+                 `("ODIN_ROOT" = (,share)))))))))
     (native-inputs
      (list llvm-18
            which
-           python-minimal))
+           python-minimal
+           gcc))  ; For vendor compilation
     (inputs
-     (list clang-toolchain-18          ; Use the complete toolchain
-           gnu-make))                  ; For vendor library compilation
-    (propagated-inputs
-     (list clang-18))                  ; Needed for runtime compilation
+     (list clang-toolchain-18  ; Complete toolchain for runtime compilation
+           clang-18
+           gnu-make))
     (home-page "https://odin-lang.org/")
     (synopsis "Fast, concise, readable, pragmatic programming language")
     (description
