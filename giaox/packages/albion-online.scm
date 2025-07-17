209aee84509ac891d0b71f644d2e4c670d3f0a6a
miaoles <iao_mm@pm.me>
2025-07-16 20:01:05 -0400

    initial albion #2

--- giaox/packages/albion-online.scm
+++ giaox/packages/albion-online.scm
@@ -4,23 +4,27 @@
   #:use-module (guix build-system trivial)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages gcc)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages elf)
   #:use-module (gnu packages gtk)
-  #:use-module (gnu packages bash)
   #:use-module (gnu packages gl)
+  #:use-module (gnu packages linux)
   #:use-module (gnu packages xorg)
   #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages nss)
   #:use-module (gnu packages cups)
-  #:use-module (gnu packages glib))
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages xml))
 
 (define-public albion-online
   (package
     (name "albion-online")
-    (version "1.0")  ; Update this to match the actual version
+    (version "1.0")
     (source (origin
               (method url-fetch)
               (uri "https://live.albiononline.com/clients/20250712102808/albion-online-setup")
@@ -36,9 +40,14 @@
          (use-modules (guix build utils))
          (let* ((out (assoc-ref %outputs "out"))
                 (bin (string-append out "/bin"))
+                (share (string-append out "/share"))
+                (applications (string-append share "/applications"))
+                (icons (string-append share "/icons/hicolor/128x128/apps"))
+                (doc (string-append share "/doc/albion-online"))
                 (libexec (string-append out "/libexec"))
                 (setup-bin (string-append libexec "/albion-online-setup"))
-                (wrapper (string-append bin "/albion-online-setup"))
+                (launcher-script (string-append bin "/albion-online-setup"))
+                (game-launcher-script (string-append bin "/albion-online"))
                 (source (assoc-ref %build-inputs "source"))
                 (patchelf (string-append (assoc-ref %build-inputs "patchelf") "/bin/patchelf"))
                 (glibc (assoc-ref %build-inputs "glibc")))
@@ -46,6 +55,9 @@
            ;; Create directories
            (mkdir-p bin)
            (mkdir-p libexec)
+           (mkdir-p applications)
+           (mkdir-p icons)
+           (mkdir-p doc)
 
            ;; Copy the binary
            (copy-file source setup-bin)
@@ -56,8 +68,8 @@
                    (string-append glibc "/lib/ld-linux-x86-64.so.2")
                    setup-bin)
 
-           ;; Create wrapper script
-           (call-with-output-file wrapper
+           ;; Create installer wrapper script
+           (call-with-output-file launcher-script
              (lambda (port)
                (format port "#!~a/bin/sh
 export LD_LIBRARY_PATH=\"~a\"
@@ -75,12 +87,98 @@ exec \"~a\" \"$@\"
                                "mesa" "pulseaudio" "gtk+" "gdk-pixbuf"
                                "cairo" "pango" "freetype" "fontconfig"
                                "nspr" "nss" "atk" "at-spi2-core" "dbus"
-                               "glib" "cups" "libxscrnsaver"))
+                               "glib" "cups" "libxscrnsaver"
+                               "libxcb" "libxau" "libxdmcp" "alsa-lib" "expat"))
                         ":")
                        (string-append (assoc-ref %build-inputs "fontconfig") "/etc/fonts")
                        (string-append (assoc-ref %build-inputs "fontconfig") "/etc/fonts/fonts.conf")
                        setup-bin)))
-           (chmod wrapper #o755)
+           (chmod launcher-script #o755)
+
+           ;; Create game launcher script
+           (call-with-output-file game-launcher-script
+             (lambda (port)
+               (format port "#!~a/bin/sh
+# Check if the game is installed
+GAME_DIR=\"$HOME/Sandbox/Games/AlbionOnline\"
+if [ ! -d \"$GAME_DIR\" ]; then
+  echo \"Albion Online is not installed. Please run albion-online-setup first.\"
+  exit 1
+fi
+
+# Add required libraries from Guix packages
+export LD_LIBRARY_PATH=\"$GAME_DIR/launcher:~a:$LD_LIBRARY_PATH\"
+
+# Set font configuration
+export FONTCONFIG_PATH=\"~a\"
+export FONTCONFIG_FILE=\"~a\"
+
+# Set Qt environment variables
+export QT_QPA_PLATFORM_PLUGIN_PATH=\"$GAME_DIR/launcher/plugins/platforms\"
+export QT_PLUGIN_PATH=\"$GAME_DIR/launcher/plugins/\"
+export QT_SCALE_FACTOR=1
+export QT_QPA_PLATFORM=\"xcb\"
+
+# Graphics settings
+export LIBGL_ALWAYS_SOFTWARE=1
+export QSG_INFO=1
+
+# Run the game
+cd \"$GAME_DIR\"
+exec \"$GAME_DIR/launcher/Albion-Online\" \"--no-sandbox\" \"-loglevel 0\" \"$@\"
+"
+                       (assoc-ref %build-inputs "bash")
+                       (string-join
+                        (map (lambda (lib)
+                               (string-append (assoc-ref %build-inputs lib) "/lib"))
+                             '("glibc" "gcc:lib" "libx11" "libxext" "libxrender"
+                               "libxtst" "libxi" "libxrandr" "libxcursor"
+                               "libxcomposite" "libxdamage" "libxfixes"
+                               "mesa" "pulseaudio" "gtk+" "gdk-pixbuf"
+                               "cairo" "pango" "freetype" "fontconfig"
+                               "nspr" "nss" "atk" "at-spi2-core" "dbus"
+                               "glib" "cups" "libxscrnsaver"
+                               "libxcb" "libxau" "libxdmcp" "alsa-lib" "expat"))
+                        ":")
+                       (string-append (assoc-ref %build-inputs "fontconfig") "/etc/fonts")
+                       (string-append (assoc-ref %build-inputs "fontconfig") "/etc/fonts/fonts.conf"))))
+           (chmod game-launcher-script #o755)
+
+           ;; Create desktop file
+           (call-with-output-file (string-append applications "/albion-online.desktop")
+             (lambda (port)
+               (format port "[Desktop Entry]
+Name=Albion Online
+Comment=Sandbox MMORPG set in an open medieval fantasy world
+Exec=albion-online
+Terminal=false
+Type=Application
+Categories=Game;RolePlaying;
+Keywords=game;mmorpg;rpg;
+Icon=albion-online
+")))
+
+           ;; Create a placeholder icon (will be replaced by the installer)
+           (call-with-output-file (string-append icons "/albion-online.png")
+             (lambda (port)
+               (display "This is a placeholder. The actual icon will be installed by the game installer." port)))
+
+           ;; Create README file
+           (call-with-output-file (string-append doc "/README")
+             (lambda (port)
+               (display "Albion Online for Guix
+
+Installation:
+1. Run 'albion-online-setup' to install the game
+2. Choose ~/Sandbox/Games/AlbionOnline as the installation directory
+3. Complete the installation process
+
+Playing:
+1. Run 'albion-online' to start the game
+
+Troubleshooting:
+If you encounter issues, try running the game from a terminal to see error messages.
+" port)))
 
            #t))))
     (native-inputs
@@ -100,6 +198,9 @@ exec \"~a\" \"$@\"
        ("libxdamage" ,libxdamage)
        ("libxfixes" ,libxfixes)
        ("libxscrnsaver" ,libxscrnsaver)
+       ("libxcb" ,libxcb)
+       ("libxau" ,libxau)
+       ("libxdmcp" ,libxdmcp)
        ("mesa" ,mesa)
        ("pulseaudio" ,pulseaudio)
        ("gtk+" ,gtk+)
@@ -115,13 +216,15 @@ exec \"~a\" \"$@\"
        ("dbus" ,dbus)
        ("glib" ,glib)
        ("cups" ,cups)
+       ("alsa-lib" ,alsa-lib)
+       ("expat" ,expat)
        ("zlib" ,zlib)))
     (home-page "https://albiononline.com")
     (synopsis "Sandbox MMORPG set in an open medieval fantasy world")
     (description
      "Albion Online is a sandbox MMORPG set in an open medieval fantasy world.
 It features a player-driven economy, classless combat system, and intense PvP battles.
-This package provides the game launcher and installer.")
+This package provides both the game installer and a launcher for the installed game.")
     (license (license:non-copyleft "https://albiononline.com/en/terms_and_conditions"))))
 
 ;; This allows building with "guix build -f"
